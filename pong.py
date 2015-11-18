d0b5d73b91da3d78a0223207ba8768c2273ba2d3
pedronogueiranunes <pedronogueiranunes@gmail.com>
2015-11-18 10:36:12 -0300

    Criada classe da bola em pong.py

---
+++ pong.py
@@ -0,0 +1,26 @@
+__author__ = 'Pedro'
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget): #classe do Jogo que recebe uma widget
+    pass
+
+class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy
+    def build(self): #funcao que retorna a instancia
+        return PongGame()
+class PongBall(Widget):
+    #velocidade da bola nos eixos X e Y
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    #referencelistproperty para juntar os valores de velocidade em uma instancia so
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    #funcao move ira mover a bola em um passo. Sera chamada em intervalos iguais para animar a bola
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+if __name__ == '__main__': #condicional que seleciona o nome
+    PongApp().run() #metodo do Kivy que roda a instancia do App
