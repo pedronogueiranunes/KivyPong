a60c0c4b529fabe3dc6b8965c37165ec3801a4af
pedronogueiranunes <pedronogueiranunes@gmail.com>
2015-11-18 13:40:45 -0300

    Adicionada Logica de Input/movimentacao dos players

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ __author__ = 'Pedro'
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self,ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y)/(self.height / 2)
+            bounced = Vector(-1*vx,vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
 
 
 class PongBall(Widget):
@@ -19,11 +32,23 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
-class PongGame(Widget): #classe do Jogo que recebe uma widget
+class PongGame(Widget): #classe do Jogo que recebe uma widget -------------------
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    def serve_ball(self, vel=(4,0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
 
     def update(self,dt):
         self.ball.move()
+
+        #movimentacao das paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+
         #volta se bater em cima e embaixo
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
@@ -32,7 +57,21 @@ class PongGame(Widget): #classe do Jogo que recebe uma widget
         if (self.ball.x < 0) or (self.ball.right > self.width):
             self.ball.velocity_x *= -1
 
-class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy
+        #SCORE --------------------
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4,0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(4,0))
+
+    def on_touch_move(self,touch):
+        if touch.x < self.width/3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
+
+class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy -----------------------
     def build(self): #funcao que retorna a instancia
         game = PongGame()
         framerate = 60.0
