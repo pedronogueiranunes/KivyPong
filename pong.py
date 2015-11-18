15bc4974a7f28012dc72be4a4dec31c8fb522b8e
pedronogueiranunes <pedronogueiranunes@gmail.com>
2015-11-18 10:42:02 -0300

    Bola adicionada a pong.kv, imports arrumados em pong.py

--- pong.py
+++ pong.py
@@ -2,13 +2,9 @@ __author__ = 'Pedro'
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
-class PongGame(Widget): #classe do Jogo que recebe uma widget
-    pass
-
-class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy
-    def build(self): #funcao que retorna a instancia
-        return PongGame()
 class PongBall(Widget):
     #velocidade da bola nos eixos X e Y
     velocity_x = NumericProperty(0)
@@ -20,6 +16,13 @@ class PongBall(Widget):
     #funcao move ira mover a bola em um passo. Sera chamada em intervalos iguais para animar a bola
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
+
+class PongGame(Widget): #classe do Jogo que recebe uma widget
+    pass
+
+class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy
+    def build(self): #funcao que retorna a instancia
+        return PongGame()
 
 
 if __name__ == '__main__': #condicional que seleciona o nome
