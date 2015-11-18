d6d4d44d24bf3fcdcda022a97ca930b37cd86c6d
pedronogueiranunes <pedronogueiranunes@gmail.com>
2015-11-18 11:42:37 -0300

    pong.py renomeado para main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 
+
+
 class PongBall(Widget):
     #velocidade da bola nos eixos X e Y
     velocity_x = NumericProperty(0)
@@ -18,11 +20,24 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget): #classe do Jogo que recebe uma widget
-    pass
+    ball = ObjectProperty(None)
+
+    def update(self,dt):
+        self.ball.move()
+        #volta se bater em cima e embaixo
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        #volta se bater nas laterais
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 class PongApp(App): #classe do Jogo que recebe uma instancia de App do Kivy
     def build(self): #funcao que retorna a instancia
-        return PongGame()
+        game = PongGame()
+        framerate = 60.0
+        Clock.schedule_interval(game.update, 1.0/framerate)
+        return game
 
 
 if __name__ == '__main__': #condicional que seleciona o nome
